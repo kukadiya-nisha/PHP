c91fc9713c662b55aef6957378b44949a64af59c
nisha kukadiya <165984021+kukadiya-nisha@users.noreply.github.com>
2025-03-11 11:33:32 +0530

    dynamic slider in index page

--- index.php
+++ index.php
@@ -1,34 +1,74 @@
-<?php include_once 'header.php'; ?>
+<?php include_once 'header.php';
+$select = "select * from slider";
+$table=mysqli_query($con,$select);
+?>
 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="./images/image.jpg" class="d-block w-100" alt="First slide" style="height: 500px;">
+<div class="carousel-indicators">
+<?php
+$i=0;
+while($row=$table->fetch_assoc())
+{
+    if($i==0)
+    {
+        ?>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="<?= $i ?>" class="active" aria-current="true" aria-label="Slide <?=$i?>"></button>
+        <?php 
+    }
+    else 
+    {
+        ?>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="<?= $i ?>" aria-label="Slide <?=$i?>"></button>
+        <?php
+    }
+    ?>
+
+    <?php 
+    $i++;
+}
+?>
+</div>
+<?php
+$select = "select * from slider";
+$table=mysqli_query($con,$select);
+?>
+<br><br>
+<div class="carousel-inner bg-info">
+<?php
+$i=0;
+while($row=$table->fetch_assoc())
+{
+    if($i==0)
+    {
+        ?>
+            <div class="carousel-item active bg-info">
+            <img src="./images/slider_images/<?=$row['slider_image']?>" class="d-block w-100" alt="<?=$i?> slide" style="height: 500px;">
             <div class="carousel-caption d-none d-md-block">
                 <h5>Special Offers</h5>
                 <p>Check out our latest deals and promotions</p>
             </div>
         </div>
-        <div class="carousel-item">
-            <img src="./images/j2zi31lqofw1abqfrjux.jpg" class="d-block w-100" alt="Second slide" style="height: 500px;">
+        <?php 
+    }
+    else 
+    {
+        ?>
+            <div class="carousel-item">
+            <img src="./images/slider_images/<?=$row['slider_image']?>" class="d-block w-100" alt="<?=$i?>  slide" style="height: 500px;">
             <div class="carousel-caption d-none d-md-block">
                 <h5>New Arrivals</h5>
                 <p>Discover our newest products</p>
             </div>
         </div>
-        <div class="carousel-item">
-            <img src="./images/Product_of_the_Year_USA_2017_Winners.jpg" class="d-block w-100" alt="Third slide" style="height: 500px;">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Free Shipping</h5>
-                <p>On orders over $50</p>
-            </div>
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <?php
+    }
+    ?>
+
+    <?php 
+    $i++;
+}
+?>
+</div>
+<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
     </button>
